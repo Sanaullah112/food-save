cb59c1c21f18eceddfc68b6f2d621b98cb2c394d
Sanaullah <sanaarman033@gmail.com>
2026-06-18 05:32:53 -0700

    Add the Render URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' })
+const API = axios.create({ baseURL: 'https://foodsave-backend-d3ss.onrender.com/api' })
 
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem('foodsave_user')
